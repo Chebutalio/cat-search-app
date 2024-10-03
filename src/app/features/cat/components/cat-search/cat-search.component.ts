b3e223937c4514082caa26d1a74140d1681fa695
chebutalio <chebutalio@gmail.com>
2024-10-03 11:52:29 +0300

    created project, added libraries, created components

---
+++ src/app/features/cat/components/cat-search/cat-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './cat-search.component.html',
+  styleUrl: './cat-search.component.css'
+})
+export class CatSearchComponent {
+
+}
